925f7aee94ad76f3734f0b3951c835fff77e2381
Rajan Kumar <raorajan9576@gmail.com>
2024-09-04 00:19:55 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <div >
+    <h1 className="text-3xl underline">Home Page</h1>
+    <Button>Add</Button>
+  </div>;
+}
+
