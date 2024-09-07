403c4b538dca4b386bd938a75a673f39a7c33012
Rajan Kumar <raorajan9576@gmail.com>
2024-09-07 16:45:14 +0530

    admin page and otp created

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { PatientForm } from "@/components/forms/PatientForm";
-const Home = () => {
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -19,7 +25,7 @@ const Home = () => {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CareCampass
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
@@ -27,6 +33,7 @@ const Home = () => {
           </div>
         </div>
       </section>
+
       <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
