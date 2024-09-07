9923ae1a893b39ce3e6db6b283b960b3b5da037d
Rajan Kumar <raorajan9576@gmail.com>
2024-09-07 15:10:37 +0530

    Patient Register page created

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePluse
+              © 2024 CareCampass
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
